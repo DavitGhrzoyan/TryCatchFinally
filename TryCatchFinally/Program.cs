f0212ffb7c7950965f88d5b53f1527285c9215ed
Davit <ghrzoyandavit@gmail.com>
2022-12-14 21:42:22 +0400

    Add project files.

---
+++ TryCatchFinally/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = 5;
+                int y = x / 0;
+                Console.WriteLine("The result is" + y);
+            }
+            catch
+            {
+                Console.WriteLine("ERROR ERROR !!!!!!:  attemt devive by zero");
+            }
+            finally
+            {
+                Console.WriteLine("finish: finally");
+            }
+            Console.WriteLine("the end of code");
+        }
+    }
+}
